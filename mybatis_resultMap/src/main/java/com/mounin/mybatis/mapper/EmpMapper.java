4a644cfcd48e39c6971371b6d7253f156f4c72cb
Honvin <2696660524@qq.com>
2023-04-03 20:35:41 +0800

    搭建Mybatis框架

---
+++ mybatis_resultMap/src/main/java/com/mounin/mybatis/mapper/EmpMapper.java
@@ -0,0 +1,10 @@
+package com.mounin.mybatis.mapper;
+
+/**
+ * @ClassName: EmpMapper
+ * @Date: 2023/4/3 20:34
+ * @Author: Honvin
+ * @Description:
+ **/
+public interface EmpMapper {
+}
---
+++ mybatis_resultMap/src/main/java/com/mounin/mybatis/pojo/Dept.java
@@ -0,0 +1,46 @@
+package com.mounin.mybatis.pojo;
+
+/**
+ * @ClassName: Dept
+ * @Date: 2023/4/3 20:32
+ * @Author: Honvin
+ * @Description:
+ **/
+public class Dept {
+
+    private Integer deptId;
+
+    private String deptName;
+
+    public Dept() {
+    }
+
+    public Dept(Integer deptId, String deptName) {
+        this.deptId = deptId;
+        this.deptName = deptName;
+    }
+
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "deptId=" + deptId +
+                ", deptName='" + deptName + '\'' +
+                '}';
+    }
+}
